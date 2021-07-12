4439ad7f3a5f86f7c1c6d9e47af8b5a80fefea93
Elovard <persall2013@gmail.com>
2021-07-12 10:55:02 +0300

    change return type of printAllTransactions method to List

--- src/main/java/extractor/FileParser.java
+++ src/main/java/extractor/FileParser.java
@@ -1,8 +1,10 @@
 package extractor;
 
+import java.util.List;
+
 public interface FileParser {
     void parse(String filePath);
-    void printAllTransactions();
+    List<Transaction> printAllTransactions();
     void printTopFiveTransactions();
     void printTotals();
 }
