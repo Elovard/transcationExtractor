0ea6688b632400634caca4170edf6aa93e6a7296
Elovard <persall2013@gmail.com>
2021-07-13 11:02:31 +0300

    simplify main method and add getExtension method

--- src/main/java/extractor/Application.java
+++ src/main/java/extractor/Application.java
@@ -1,76 +1,51 @@
 package extractor;
 
-import extractor.factories.CsvFactory;
+import extractor.entity.Transaction;
 import extractor.factories.ParserFactory;
-import extractor.factories.XmlFactory;
 import extractor.parsers.FileParser;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
-    private FileParser fileParser;
-
-    public Application(ParserFactory factory) {
-        fileParser = factory.createParser();
-    }
-
-    private static Application settingUp() {
-        Application app;
-        ParserFactory factory;
-        Scanner scan = new Scanner(System.in);
-        String filePath;
-
-        System.out.println("Please specify the path to your file: ");
-        filePath = scan.nextLine();
-
-        if (filePath.endsWith(".xml")) {
-            factory = new XmlFactory();
-            app = new Application(factory);
-            app.fileParser.parse(filePath);
-            return app;
-        }
-
-        if (filePath.endsWith(".csv")) {
-            factory = new CsvFactory();
-            app = new Application(factory);
-            app.fileParser.parse(filePath);
-            return app;
-        }
-
-        System.out.println("Can't find your file!");
-        return null;
-    }
 
 
     public static void main(String[] args) {
-        Application app = settingUp();
-        if (app != null) {
-            Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-            System.out.println("Parsing xml file...");
-            System.out.println("File parsed successfully");
-            System.out.println("Select operation: ");
-            System.out.println("[1] Print data to display \n[2] Print top-5 transactions \n[3] Print totals \n[4] Exit");
+        System.out.println("Enter path: ");
+        String path = scan.nextLine();
+        String extension = getExtension(path);
 
-            int choice = scan.nextInt();
+        FileParser fileParser = ParserFactory.createParser(extension);
+        List<Transaction> transactionList = fileParser.parse(path);
 
-            switch (choice) {
-                case 1:
-                    app.fileParser.printAllTransactions();
-                    break;
-                case 2:
-                    app.fileParser.printTopFiveTransactions();
-                    break;
-                case 3:
-                    app.fileParser.printTotals();
-                    break;
-                case 4:
-                    break;
-                default:
-                    System.out.println("Invalid number");
-            }
-            return;
+        System.out.println("Parsing xml file...");
+        System.out.println("File parsed successfully");
+        System.out.println("Select operation: ");
+        System.out.println("[1] Print data to display \n[2] Print top-5 transactions \n[3] Print totals \n[4] Exit");
+
+        int choice = scan.nextInt();
+
+        switch (choice) {
+            case 1:
+                fileParser.printAllTransactions();
+                break;
+            case 2:
+                fileParser.printTopFiveTransactions();
+                break;
+            case 3:
+                fileParser.printTotals();
+                break;
+            case 4:
+                break;
+            default:
+                System.out.println("Invalid number");
         }
-        System.out.println("Error occurred during parsing");
+    }
+
+    public static String getExtension(String path) {
+        StringBuilder sb = new StringBuilder(path);
+        return sb.delete(0, sb.length() - 3).toString();
     }
 }
