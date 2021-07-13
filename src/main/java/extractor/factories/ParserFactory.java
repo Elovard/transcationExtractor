af7a2de67db1b96e30e107b17989f67204ff415b
Elovard <persall2013@gmail.com>
2021-07-13 11:00:34 +0300

    add reflection to ParserFactory method

--- src/main/java/extractor/factories/ParserFactory.java
+++ src/main/java/extractor/factories/ParserFactory.java
@@ -1,7 +1,34 @@
 package extractor.factories;
 
 import extractor.parsers.FileParser;
+import org.reflections.Reflections;
 
-public interface ParserFactory {
-    FileParser createParser();
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Set;
+
+public class ParserFactory {
+
+    public static FileParser createParser(String extension) {
+
+        Reflections reflections = new Reflections("extractor");
+        Set<Class<? extends FileParser>> classes = reflections.getSubTypesOf(FileParser.class);
+
+        for (Class<? extends FileParser> one : classes) {
+            try {
+                Method method = one.getDeclaredMethod("getSupportedFileType", null);
+                String result = (String) method.invoke(one.newInstance(), null);
+
+                if (extension.equals(result)) {
+                    return one.newInstance();
+                }
+
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Unsupported type of file");
+        return null;
+    }
+
 }
