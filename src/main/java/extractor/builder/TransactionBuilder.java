cc2a30f2ce5d9a58ec4522f2deeef46e04cf1092
Elovard <persall2013@gmail.com>
2021-07-16 13:32:13 +0300

    remove interface Builder

--- src/main/java/extractor/builder/TransactionBuilder.java
+++ src/main/java/extractor/builder/TransactionBuilder.java
@@ -5,7 +5,7 @@ import extractor.entity.TransactionStatus;
 
 import java.util.Date;
 
-public class TransactionBuilder implements Builder {
+public class TransactionBuilder {
     private Date date;
     private String transactionId;
     private String userId;
@@ -13,37 +13,31 @@ public class TransactionBuilder implements Builder {
     private String currency;
     private TransactionStatus status;
 
-    @Override
     public TransactionBuilder setDate(Date date) {
         this.date = date;
         return this;
     }
 
-    @Override
     public TransactionBuilder setTransactionId(String transactionId) {
         this.transactionId = transactionId;
         return this;
     }
 
-    @Override
     public TransactionBuilder setUserId(String userId) {
         this.userId = userId;
         return this;
     }
 
-    @Override
     public TransactionBuilder setAmount(double amount) {
         this.amount = amount;
         return this;
     }
 
-    @Override
     public TransactionBuilder setCurrency(String currency) {
         this.currency = currency;
         return this;
     }
 
-    @Override
     public TransactionBuilder setTransactionStatus(TransactionStatus status) {
         this.status = status;
         return this;
