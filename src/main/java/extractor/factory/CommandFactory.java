c5d9fb1e99fd06f85f35780a04c1d9ac3dc9a5a3
Elovard <persall2013@gmail.com>
2021-07-19 10:17:45 +0300

    add CommandFactory

---
+++ src/main/java/extractor/factory/CommandFactory.java
@@ -0,0 +1,41 @@
+package extractor.factory;
+
+import extractor.command.Command;
+import org.reflections.Reflections;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class CommandFactory {
+    public static final String ROOT_PACKAGE = "extractor.command";
+
+    private final List<Command> commands = new ArrayList<>();
+
+    public CommandFactory() {
+        Reflections reflections = new Reflections(ROOT_PACKAGE);
+        Set<Class<? extends Command>> commandImplementations = reflections.getSubTypesOf(Command.class);
+
+        int incrementalCommandId = 1;
+
+        for (Class<? extends Command> impl : commandImplementations) {
+            try {
+                Constructor<? extends Command> constructor = impl.getConstructor(Integer.TYPE);
+                Command implementation = constructor.newInstance(incrementalCommandId);
+
+                incrementalCommandId++;
+
+                commands.add(implementation);
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public List<Command> getCommands() {
+        return this.commands;
+    }
+
+}
