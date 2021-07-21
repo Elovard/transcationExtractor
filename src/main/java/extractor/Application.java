a2507ea9366a333a6759eef44fdc801ad28b2a27
Elovard <persall2013@gmail.com>
2021-07-21 11:05:21 +0300

    split method settingUp into several methods

--- src/main/java/extractor/Application.java
+++ src/main/java/extractor/Application.java
@@ -2,7 +2,7 @@ package extractor;
 
 import extractor.command.Command;
 import extractor.entity.Transaction;
-import extractor.exception.TransactionException;
+import extractor.exception.ExtensionResolvingException;
 import extractor.factory.CommandFactory;
 import extractor.factory.ParserFactory;
 import extractor.parser.FileParser;
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.List;
@@ -22,60 +23,76 @@ public class Application {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
     private final Map<Integer, Command> listOfCommands = new HashMap<>();
 
-    public void settingUp(String path) throws Exception {
-        CommandFactory commandFactory = new CommandFactory();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public Map<String, String> readCommandLineArguments(String[] args) throws ExtensionResolvingException {
+        int filesFound = 0;
+        Map<String, String> arguments = new HashMap<>();
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].startsWith("file=")) {
+                String[] filePath = args[i].split("=");
+                String key = filePath[0];
+                String value = filePath[1];
+                arguments.put(key, value);
+                filesFound++;
+                logger.info("found a file");
+            }
+        }
+        if (filesFound > 1) {
+            logger.error("found more than one file");
+            throw new ExtensionResolvingException("Find more than one file!");
+        }
+        if (filesFound == 0) {
+            logger.error("no files found");
+            throw new ExtensionResolvingException("Can't find your file!");
+        }
+        System.out.println(arguments.get("file") + " is a file");
+        return arguments;
+    }
 
-        logger.info("received path from console");
-        String extension = Application.getExtension(path);
+    public FileParser resolveParser(String extension) throws ExtensionResolvingException {
         logger.info("received extension from method getExtension");
-
         ParserFactory factory = new ParserFactory();
         FileParser fileParser = factory.createParser(extension);
 
         if (fileParser == null) {
             logger.error("received unsupported type of file!");
-            throw new TransactionException("Unsupported type of file!");
+            throw new ExtensionResolvingException("Unsupported type of file!");
         }
 
         logger.info("created parser");
-        List<Transaction> transactionList = null;
-        transactionList = fileParser.parse(path);
+        return fileParser;
+    }
 
+    public List<Command> getCommands() {
+        CommandFactory commandFactory = new CommandFactory();
+        logger.info("initializing list of commands");
+        return commandFactory.getCommands();
+    }
 
-        logger.info("successfully parsed file");
-        System.out.println("Parsing your file...");
-        System.out.println("File parsed successfully");
-
-        initCommandList(commandFactory.getCommands());
+    public int readUserInput() throws IOException {
+        System.out.println("Select operation: ");
         printCommands();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String choiceAsString = reader.readLine();
+        return Integer.parseInt(choiceAsString);
+    }
 
-        while (true) {
-            System.out.println("Select operation: ");
-            String choiceAsString = reader.readLine();
-            int choice = Integer.parseInt(choiceAsString);
-
-            if (choice == 0) {
-                break;
-            }
-
-            try {
-                Command command = listOfCommands.get(choice);
-                command.execute(transactionList);
-            } catch (NullPointerException ex) {
-                System.out.println("Invalid command");
-                logger.error("received invalid command from input");
-            }
+    public void executeCommand(int userInput, List<Transaction> transactions) {
+        try {
+            Command command = listOfCommands.get(userInput);
+            command.execute(transactions);
+        } catch (NullPointerException ex) {
+            System.out.println("Invalid command");
+            logger.error("received invalid command from input");
         }
     }
 
-    public static String getExtension(String path) throws Exception {
+    public String getExtension(String path) throws Exception {
         Pattern pattern = Pattern.compile("\\.([\\w]+)$");
         Matcher matcher = pattern.matcher(path);
         String result = null;
         if (path.isEmpty()) {
             logger.error("invalid file name");
-            throw new Exception("Invalid file name");
+            throw new ExtensionResolvingException("Invalid file name");
         }
         if (matcher.find()) {
             result = matcher.group(1);
--- src/main/java/extractor/Program.java
+++ src/main/java/extractor/Program.java
@@ -1,14 +1,36 @@
 package extractor;
 
-import extractor.exception.TransactionException;
+import extractor.command.Command;
+import extractor.entity.Transaction;
+import extractor.exception.ApplicationException;
+import extractor.parser.FileParser;
+
+import java.util.List;
+import java.util.Map;
 
 public class Program {
 
     public static void main(String[] args) throws Exception {
         Application app = new Application();
         try {
-            app.settingUp(args[0]);
-        } catch (TransactionException ex) {
+            Map<String, String> arguments = app.readCommandLineArguments(args);
+            String filePath = arguments.get("file");
+            String extension = app.getExtension(filePath);
+            FileParser parser = app.resolveParser(extension);
+            List<Transaction> transactions = parser.parse(filePath);
+            System.out.println("Parsing your file...");
+            System.out.println("File parsed successfully");
+            List<Command> commands = app.getCommands();
+
+            while (true) {
+                app.initCommandList(commands);
+                int userInput = app.readUserInput();
+                if (userInput == 0) {
+                    break;
+                }
+                app.executeCommand(userInput, transactions);
+            }
+        } catch (ApplicationException ex) {
             System.out.println("Error occurred during execution of the program" + "\n" + ex.getMessage());
         }
     }
