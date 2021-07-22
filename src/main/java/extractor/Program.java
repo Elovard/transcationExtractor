a3164a10444f7c5d3246de64e9f71ba659f843b5
Elovard <persall2013@gmail.com>
2021-07-22 14:10:42 +0300

    add method initCommands and apply new constant

--- src/main/java/extractor/Program.java
+++ src/main/java/extractor/Program.java
@@ -1,6 +1,5 @@
 package extractor;
 
-import extractor.command.Command;
 import extractor.entity.Transaction;
 import extractor.exception.ApplicationException;
 import extractor.parser.FileParser;
@@ -14,21 +13,28 @@ public class Program {
         Application app = new Application();
         try {
             Map<String, String> arguments = app.readCommandLineArguments(args);
+
             String filePath = arguments.get("file");
+
             String extension = app.getExtension(filePath);
+
             FileParser parser = app.resolveParser(extension);
-            List<Transaction> transactions = parser.parse(filePath);
+
             System.out.println("Parsing your file...");
+            List<Transaction> transactions = parser.parse(filePath);
+
             System.out.println("File parsed successfully");
-            List<Command> commands = app.getCommands();
+
+            app.initCommands();
 
             while (true) {
-                app.initCommandList(commands);
-                int userInput = app.readUserInput();
-                if (userInput == 0) {
+                app.printCommands();
+
+                int commandId = app.readUserInput();
+                if (Application.EXIT_COMMAND_ID == commandId) {
                     break;
                 }
-                app.executeCommand(userInput, transactions);
+                app.executeCommand(commandId, transactions);
             }
         } catch (ApplicationException ex) {
             System.out.println("Error occurred during execution of the program" + "\n" + ex.getMessage());
