54b00bffc8b09997e421a3a242d68ca0bdbd11bb
Elovard <persall2013@gmail.com>
2021-07-22 14:09:42 +0300

    simplify Application class and unite methods getCommands and initCommandList

--- src/main/java/extractor/Application.java
+++ src/main/java/extractor/Application.java
@@ -2,6 +2,7 @@ package extractor;
 
 import extractor.command.Command;
 import extractor.entity.Transaction;
+import extractor.exception.ApplicationException;
 import extractor.exception.ExtensionResolvingException;
 import extractor.factory.CommandFactory;
 import extractor.factory.ParserFactory;
@@ -20,31 +21,32 @@ import java.util.regex.Pattern;
 
 public class Application {
 
+    public static final int EXIT_COMMAND_ID = 0;
+
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
-    private final Map<Integer, Command> listOfCommands = new HashMap<>();
+    private final Map<Integer, Command> commands = new HashMap<>();
 
     public Map<String, String> readCommandLineArguments(String[] args) throws ExtensionResolvingException {
-        int filesFound = 0;
         Map<String, String> arguments = new HashMap<>();
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].startsWith("file=")) {
-                String[] filePath = args[i].split("=");
-                String key = filePath[0];
-                String value = filePath[1];
-                arguments.put(key, value);
-                filesFound++;
-                logger.info("found a file");
+        for (String argument : args) { // file=/file.csv lol=kek=kek vlad=molodec currencyRate=1.345 currencyCode=eur
+            String[] argumentParts = argument.split("=");
+
+            if (argumentParts.length != 2) {
+                logger.warn("Found invalid argument {}, skipping it and moving to the next one", argument);
+                continue;
             }
+
+            String argumentName = argumentParts[0];
+            String argumentValue = argumentParts[1];
+
+            if (arguments.containsKey(argumentName)) {
+                logger.warn("Found duplicate argument with name: {}, argument {} will be ignored", argumentName, argument);
+                continue;
+            }
+
+            arguments.put(argumentName, argumentValue);
         }
-        if (filesFound > 1) {
-            logger.error("found more than one file");
-            throw new ExtensionResolvingException("Find more than one file!");
-        }
-        if (filesFound == 0) {
-            logger.error("no files found");
-            throw new ExtensionResolvingException("Can't find your file!");
-        }
-        System.out.println(arguments.get("file") + " is a file");
+
         return arguments;
     }
 
@@ -62,55 +64,57 @@ public class Application {
         return fileParser;
     }
 
-    public List<Command> getCommands() {
-        CommandFactory commandFactory = new CommandFactory();
-        logger.info("initializing list of commands");
-        return commandFactory.getCommands();
-    }
-
-    public int readUserInput() throws IOException {
+    public int readUserInput() throws ApplicationException {
         System.out.println("Select operation: ");
-        printCommands();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String choiceAsString = reader.readLine();
-        return Integer.parseInt(choiceAsString);
-    }
 
-    public void executeCommand(int userInput, List<Transaction> transactions) {
         try {
-            Command command = listOfCommands.get(userInput);
-            command.execute(transactions);
-        } catch (NullPointerException ex) {
-            System.out.println("Invalid command");
-            logger.error("received invalid command from input");
+            String choiceAsString = reader.readLine();
+            return Integer.parseInt(choiceAsString);
+        } catch (IOException | NumberFormatException e) {
+            throw new ApplicationException("An error occured while reading user's input: " + e.getMessage());
         }
     }
 
-    public String getExtension(String path) throws Exception {
+    public void executeCommand(int commandId, List<Transaction> transactions) throws ApplicationException {
+        if (!commands.containsKey(commandId)) {
+           throw new ApplicationException("Command with given id: " + commandId + " not found");
+        }
+
+        Command command = commands.get(commandId);
+        command.execute(transactions);
+    }
+
+    public String getExtension(String path) throws ExtensionResolvingException {
+        if (path == null || path.isEmpty()) {
+            logger.error("Invalid file path: {}", path);
+            throw new ExtensionResolvingException("Invalid file path: " + path);
+        }
+
         Pattern pattern = Pattern.compile("\\.([\\w]+)$");
         Matcher matcher = pattern.matcher(path);
-        String result = null;
-        if (path.isEmpty()) {
-            logger.error("invalid file name");
-            throw new ExtensionResolvingException("Invalid file name");
+
+        if (!matcher.find()) {
+            throw new ExtensionResolvingException("Unable to resolve extension from path: " + path);
         }
-        if (matcher.find()) {
-            result = matcher.group(1);
-        }
-        return result;
+
+        return matcher.group(1);
     }
 
     public void printCommands() {
         System.out.println("The List of available commands: \n[0]Exit");
-        for (Command command : listOfCommands.values()) {
+        for (Command command : commands.values()) {
             System.out.println(command);
         }
     }
 
-    public void initCommandList(List<Command> commands) {
-        logger.info("initializing the list of commands");
-        for (Command command : commands) {
-            listOfCommands.put(command.getCommandId(), command);
+    public void initCommands() {
+        CommandFactory commandFactory = new CommandFactory(); // make CommandFactory as singleton
+        logger.info("initializing list of commands");
+        List<Command> commandList = commandFactory.getCommands();
+
+        for (Command command : commandList) {
+            this.commands.put(command.getCommandId(), command);
         }
     }
 }
