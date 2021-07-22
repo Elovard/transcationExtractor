c0bd7afbb9b9bf4d20ca71985f106280307ecdda
Elovard <persall2013@gmail.com>
2021-07-22 15:08:31 +0300

    add tests for Application class

--- src/test/java/extractor/ApplicationTest.java
+++ src/test/java/extractor/ApplicationTest.java
@@ -1,33 +1,61 @@
 package extractor;
 
+import extractor.entity.Transaction;
+import extractor.exception.ApplicationException;
+import extractor.exception.ExtensionResolvingException;
+import extractor.parser.FileParser;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationTest {
 
+    public Application app = new Application();
+
     @Test
-    void whenFileNameIsCorrect_ThenResultShouldBeTrue() {
-        String path = "csv_example.csv";
-        String expectedExtension = "csv";
-        Application app = new Application();
-        try {
-            assertEquals(expectedExtension, app.getExtension(path));
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+    void whenCommandLineArgumentsContainMoreThanOneCommand_ThenOnlyOneArgumentShouldBeSaved()
+            throws ExtensionResolvingException {
+
+        String[] commandLineArguments = {"file=test.csv", "file=file.xml", "file=sample.dat"};
+        Map<String, String> parsedArguments = app.parseCommandLineArguments(commandLineArguments);
+        assertEquals(1, parsedArguments.size());
     }
 
     @Test
-    void whenFileNameIsEmpty_ThenExceptionShouldBeThrown() {
-        String pathCsv = "";
-        Application app = new Application();
-        try {
-           app.getExtension(pathCsv);
-        } catch (Exception e) {
-            assertThrows(Exception.class, () -> app.getExtension(pathCsv));
-        }
+    void whenArgumentsAreInvalid_ThenTheyShouldNotBeSaved() throws ExtensionResolvingException {
+        String[] commandLineArguments = {"file=file=file", "test=test=test=test=test", "sample"};
+        Map<String, String> parsedArguments = app.parseCommandLineArguments(commandLineArguments);
+        assertEquals(0, parsedArguments.size());
+    }
+
+    @Test
+    void whenFileTypeIsSupported_ThenParserShouldBeCreated() throws ExtensionResolvingException {
+        String extension = "csv";
+        FileParser fileParser = app.resolveParser(extension);
+        assertNotNull(fileParser);
+    }
+
+    @Test
+    void whenFileTypeUnsupported_ThenExceptionShouldBeThrown() {
+        String extension = "test";
+        assertThrows(ExtensionResolvingException.class, () -> app.resolveParser(extension));
+    }
+
+    @Test
+    void whenExtensionIsNull_ThenExceptionShouldBeThrown() {
+        String extension = null;
+        assertThrows(ExtensionResolvingException.class, () -> app.resolveParser(extension));
+    }
+
+    @Test
+    void whenCommandIsNotFound_ThenExceptionShouldBeThrown() throws ApplicationException {
+        int noSuchCommandId = 10;
+        List<Transaction> transactions = new ArrayList<>();
+        assertThrows(ApplicationException.class, () -> app.executeCommand(noSuchCommandId, transactions));
     }
 
 }
