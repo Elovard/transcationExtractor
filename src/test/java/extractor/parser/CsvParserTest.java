461f718d12241defb0f625d89af105d06060fe0f
Elovard <persall2013@gmail.com>
2021-07-22 15:05:22 +0300

    add tests for Csv and Xml parsers

--- src/test/java/extractor/parser/CsvParserTest.java
+++ src/test/java/extractor/parser/CsvParserTest.java
@@ -1,11 +1,15 @@
 package extractor.parser;
 
 import extractor.entity.Transaction;
+import extractor.entity.TransactionStatus;
+import extractor.exception.TransactionFileParsingException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -17,8 +21,8 @@ class CsvParserTest {
     void whenCsvFileExists_ThenResultShouldBeTrue() throws Exception {
         String pathCsv = "csv_example.csv";
         CsvParser csvParser = new CsvParser();
-        List<Transaction> transactionListOfCsv = csvParser.parse(pathCsv);
-        assertNotNull(transactionListOfCsv);
+        List<Transaction> transactions = csvParser.parse(pathCsv);
+        assertNotNull(transactions);
     }
 
     @Test
@@ -31,7 +35,6 @@ class CsvParserTest {
     @ParameterizedTest
     @MethodSource
     void whenGivenLineHas6Parameters_ThenParseShouldBeSuccessful(String line) {
-        CsvParser csvParser = new CsvParser();
         String[] parsedData = line.split(",");
         int expectedSizeOfLine = 6;
         int actualSizeOfLine = parsedData.length;
@@ -42,14 +45,13 @@ class CsvParserTest {
         return Stream.of(
                 Arguments.of("1619674154,1c22f114-8251-404e-8c3f-73b70bd0ec80,a00a7fb0-3a72-454d-865d-8f6818f8dd62,34623,usd,success"),
                 Arguments.of("1619674154,1c22f114-8251-404e-8c3f-73b70bd0ec80,a00a7fb0-3a72-454d-865d-8f6818f8dd62,34623,usd, rejected"),
-                Arguments.of("1619674154,1c22f114-8251-404e-8c3f-73b70bd0ec80,a00a7fb0-3a72-454d-865d-8f6818f8dd62,34623, euro, failed")
+                Arguments.of("1619674154,1c22f114-8251-404e-8c3f-73b70bd0ec80,a00a7fb0-3a72-454d-865d-8f6818f8dd62,34623, eur, failed")
         );
     }
 
     @ParameterizedTest
     @MethodSource
     void whenGivenLineHasLessOrMoreThan6Parameters_ThenParseShouldFail(String line) {
-        CsvParser csvParser = new CsvParser();
         String[] parsedData = line.split(",");
         int expectedSizeOfLine = 6;
         int actualSizeOfLine = parsedData.length;
@@ -63,6 +65,25 @@ class CsvParserTest {
                 Arguments.of("1619674154,1c22f114-8251-404e-8c3f-73b70bd0ec80,a00a7fb0-3a72-454d-865d-8f6818f8dd62,34623,usd"),
                 Arguments.of("1619674154,1c22f114-8251-404e-8c3f-73b70bd0ec80,a00a7fb0-3a72-454d-865d-8f6818f8dd62,34623,success")
         );
+    }
+
+    @Test
+    void whenFileExists_ThenAllFieldsShouldBeSetCorrectly() throws TransactionFileParsingException {
+        String path = "src/test/resources/sample/csv_sample.csv";
+        CsvParser parser = new CsvParser();
+        List<Transaction> transactions = parser.parse(path);
+        Transaction transaction = transactions.get(0);
+
+        long parsedLong = Long.parseLong("1619677426");
+        Timestamp timestamp = new Timestamp(parsedLong);
+        Date date = new Date(timestamp.getTime());
+
+        assertEquals(date, transaction.getTimestamp());
+        assertEquals("11111", transaction.getTransactionId());
+        assertEquals("22222", transaction.getUserId());
+        assertEquals(150, transaction.getAmount());
+        assertEquals("usd", transaction.getCurrency());
+        assertEquals(TransactionStatus.FAILED, transaction.getTransactionResult());
     }
 
 }
--- src/test/java/extractor/parser/XmlParserTest.java
+++ src/test/java/extractor/parser/XmlParserTest.java
@@ -1,12 +1,16 @@
 package extractor.parser;
 
 import extractor.entity.Transaction;
+import extractor.entity.TransactionStatus;
+import extractor.exception.TransactionFileParsingException;
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class XmlParserTest {
 
@@ -25,4 +29,30 @@ class XmlParserTest {
         assertThrows(Exception.class, () -> xmlParser.parse(pathXml));
     }
 
+    @Test
+    void whenFileExists_ThenAllFieldsShouldBeSetCorrectly() throws TransactionFileParsingException, ParseException {
+        String path = "src/test/resources/sample/sample.xml";
+        XmlParser parser = new XmlParser();
+        List<Transaction> transactions = parser.parse(path);
+        Transaction transaction = transactions.get(0);
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-M-dd hh:mm:ss");
+        Date date = format.parse("2021-04-29 16:46:13");
+
+        assertEquals("11111", transaction.getTransactionId());
+        assertEquals("22222", transaction.getUserId());
+        assertEquals(date, transaction.getTimestamp());
+        assertEquals(100, transaction.getAmount());
+        assertEquals("USD", transaction.getCurrency());
+        assertEquals(TransactionStatus.COMPLETE, transaction.getTransactionResult());
+    }
+
+    @Test
+    void whenTransactionIdIsNull_ThenResultShouldBeEmpty() throws TransactionFileParsingException {
+        String path = "src/test/resources/sample/sample2.xml";
+        XmlParser parser = new XmlParser();
+        List<Transaction> transactions = parser.parse(path);
+        Transaction transaction = transactions.get(0);
+        assertTrue(transaction.getTransactionId().isEmpty());
+    }
 }
