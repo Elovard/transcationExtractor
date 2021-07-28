e9330610f3bb7d7d599c96480503956ad1d7e152
Elovard <persall2013@gmail.com>
2021-07-28 10:04:03 +0300

    add tests for ParserFactory

--- src/test/java/extractor/factory/ParserFactoryTest.java
+++ src/test/java/extractor/factory/ParserFactoryTest.java
@@ -3,6 +3,8 @@ package extractor.factory;
 import extractor.Application;
 import extractor.parser.FileParser;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -11,28 +13,31 @@ class ParserFactoryTest {
 
     @Test
     void whenGivenExtensionIsCsv_ThenCsvParserShouldBeCreated() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         String csvExtension = "csv";
         String expectedTypeOfParser = "csv";
-        ParserFactory parserFactory = new ParserFactory();
+        ParserFactory parserFactory = (ParserFactory) context.getBean("parserFactory");
         FileParser fileParser = parserFactory.createParser(csvExtension);
         assertEquals(expectedTypeOfParser, fileParser.getSupportedFileType());
     }
 
     @Test
     void whenGivenExtensionIsXml_ThenXmlParserShouldBeCreated() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         String xmlExtension = "xml";
         String expectedTypeOfParser = "xml";
-        ParserFactory parserFactory = new ParserFactory();
+        ParserFactory parserFactory = (ParserFactory) context.getBean("parserFactory");
         FileParser fileParser = parserFactory.createParser(xmlExtension);
         assertEquals(expectedTypeOfParser, fileParser.getSupportedFileType());
     }
 
     @Test
     void whenGivenExtensionIsNotSupported_ThenExceptionShouldBeThrown() throws Exception {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Application app = context.getBean(Application.class);
         String path = "file.test";
-        Application app = new Application();
         String extension = app.getExtension(path);
-        ParserFactory parserFactory = new ParserFactory();
+        ParserFactory parserFactory = (ParserFactory) context.getBean("parserFactory");
         FileParser fileParser = parserFactory.createParser(extension);
         assertThrows(Exception.class, () -> fileParser.parse(path));
 
