bd6ae6f412320c8b215ae2b61d87fa9a72ae397a
Elovard <persall2013@gmail.com>
2021-07-28 10:08:50 +0300

    remove manual creation of Application object in Program class

--- src/main/java/extractor/Program.java
+++ src/main/java/extractor/Program.java
@@ -3,6 +3,8 @@ package extractor;
 import extractor.entity.Transaction;
 import extractor.exception.ApplicationException;
 import extractor.parser.FileParser;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
 import java.util.Map;
@@ -10,7 +12,10 @@ import java.util.Map;
 public class Program {
 
     public static void main(String[] args) throws Exception {
-        Application app = new Application();
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Application app = context.getBean(Application.class);
+
         try {
             Map<String, String> arguments = app.parseCommandLineArguments(args);
 
